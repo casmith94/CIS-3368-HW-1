fee1a1efa93f3625d56e202293eb6b3f592233e5
casmith94 <60829184+casmith94@users.noreply.github.com>
2021-06-08 18:03:37 -0500

    Add files via upload

---
+++ Christopher Smith - CIS 3368 - Homework 1/Jan25FX/src/sample/Employee.java
@@ -0,0 +1,26 @@
+package sample;
+import java.util.UUID;
+import javafx.event.*;
+
+public class Employee implements Worker{
+    public String firstName;
+    public String lastName;
+    public UUID id;
+    public boolean isActive;
+
+    @Override
+   public String toString(){
+        return this.firstName + " " + this.lastName;
+    }
+
+    @Override
+    public void hire(){
+        isActive = true;
+    }
+
+    @Override
+    public void fire(){
+        isActive = false;
+    }
+
+}
